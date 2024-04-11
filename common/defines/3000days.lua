d886c1499c331cea678bd35ede459758a403254e
loss_of_time <1399651432@qq.com>
2024-04-11 20:44:43 +0800

    整合海军快速维修、3000天科研点储存、更好补给

---
+++ common/defines/3000days.lua
@@ -0,0 +1,1 @@
+NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 3000.0,		-- Base amount of research points a country can save per slot.
---
+++ common/defines/NavalRepair_defines.lua
@@ -0,0 +1,1 @@
+NDefines.NBuildings.NAVALBASE_REPAIR_MULT = 0.15  -- 海军基地维修能力系数
